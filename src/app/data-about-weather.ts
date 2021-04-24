d622a52a5346abfb400383ec2ac649ffc2ea663f
Мария Вихляева <80171807+mviklyaeva@users.noreply.github.com>
2021-04-24 17:29:53 +0300

    Create data-about-weather.ts

---
+++ src/app/data-about-weather.ts
@@ -0,0 +1,11 @@
+import {weather} from "./weather"
+
+export const weatherData: weather[]=[
+    { day: 24 , mouth: "april", temp: 15 , prec: "rainy"},
+    { day: 25 , mouth: "april", temp: 11 , prec: "sunny"},
+    { day: 26 , mouth: "april", temp: 12 , prec: "rainy"},
+    { day: 27 , mouth: "april", temp: 17 , prec: "sunny"},
+    { day: 28 , mouth: "april", temp: 13 , prec: "cloudy"},
+    { day: 29 , mouth: "april", temp: 18 , prec: "sunny"},
+    { day: 30 , mouth: "april", temp: 19 , prec: "sunny"},
+]
